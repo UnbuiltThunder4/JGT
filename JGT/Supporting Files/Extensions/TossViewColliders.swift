652d252950e8de81cf32afdaa842d66d78da9a77
UnbuiltThunder4 <unbuiltthunder4@gmail.com>
2022-06-13 10:31:47 +0200

    melee now attack the dark son

--- JGT/Supporting Files/Extensions/TossViewColliders.swift
+++ JGT/Supporting Files/Extensions/TossViewColliders.swift
@@ -81,14 +81,11 @@ extension TossScene: SKPhysicsContactDelegate {
             if let node = firstBody.node as? DarkSon {
                 if let node2 = secondBody.node as? Enemy {
                     node2.darkTarget = node
-                    print("a")
                 }
             }
             if let node = secondBody.node as? DarkSon {
                 if let node2 = firstBody.node as? Enemy {
                     node2.darkTarget = node
-                    print("a")
-
                 }
             }
         
