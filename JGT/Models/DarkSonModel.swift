7d138f468bc71e3ba4f612e0b850436be056126a
Giuseppe Falso <giuseppeefalso@gmail.com>
2022-06-15 17:09:24 +0200

    Update to main screen animations

--- JGT/Models/DarkSonModel.swift
+++ JGT/Models/DarkSonModel.swift
@@ -34,7 +34,7 @@ class DarkSon: SKSpriteNode, Identifiable, ObservableObject {
     init() {
         super.init(texture: SKTexture(imageNamed: "darkson"), color: .red, size: CGSize(width: 233, height: 333))
         self.name = "darkson"
-        self.speed = 26.0
+        self.speed = 6.0
 //        self.position.x = self.spawnX
 //        self.position.y = self.spawnY
         
@@ -109,7 +109,7 @@ class DarkSon: SKSpriteNode, Identifiable, ObservableObject {
                 self.position.y = self.spawnY!
                 self.alpha = 0.0
                 self.respawnCounter += 1
-                self.lives -= 5
+                self.lives -= 1
                 tossScene.hud.livesCounter.text = "X \(self.lives)"
                 gameLogic.playSound(node: self.parent?.scene?.camera, audio: Audio.EffectFiles.darkSonGrunt, wait: false, muted: gameLogic.muted)
                 if self.lives == 0 {
--- JGT/Views/Main Screen/MainScreenView.swift
+++ JGT/Views/Main Screen/MainScreenView.swift
@@ -16,6 +16,16 @@ class MenuScene: SKScene {
     var mountainsFront = SKSpriteNode(imageNamed: "menu-mountains-front")
     var ground = SKSpriteNode(imageNamed: "menu-ground")
     var darkSon = SKSpriteNode(imageNamed: "darkson")
+    var goblins: [SKSpriteNode] = [
+        SKSpriteNode(imageNamed: "goblin"),
+        SKSpriteNode(imageNamed: "goblin"),
+        SKSpriteNode(imageNamed: "goblin")
+    ]
+    var goblinSpawn: [CGPoint] = [
+        CGPoint(),
+        CGPoint(),
+        CGPoint()
+    ]
     var gameTitle = SKSpriteNode(imageNamed: "title")
     
     override func didMove(to view: SKView) {
@@ -23,6 +33,7 @@ class MenuScene: SKScene {
         self.background.size = UIScreen.main.bounds.size
         self.background.anchorPoint = CGPoint(x: 0, y: 0)
         self.background.position = CGPoint(x: 0.0, y: 0.0)
+        self.background.zPosition = 1
         self.addChild(background)
         
         self.mountainsBack.name = "mountainsBack"
@@ -30,12 +41,14 @@ class MenuScene: SKScene {
         self.mountainsBack.size.height = UIScreen.main.bounds.maxY * 0.7
         self.mountainsBack.anchorPoint = CGPoint(x: 0, y: 0)
         self.mountainsBack.position = CGPoint(x: 0.0, y: 0.0)
+        self.mountainsBack.zPosition = 2
         self.addChild(mountainsBack)
         
         self.darkLordTower.name = "darkLordTower"
         self.darkLordTower.size = CGSize(width: 300, height: 600)
         self.darkLordTower.anchorPoint = CGPoint(x: 0, y: 0)
         self.darkLordTower.position = CGPoint(x: UIScreen.main.bounds.maxX * 0.05, y: UIScreen.main.bounds.maxY * 0.2)
+        self.darkLordTower.zPosition = 2
         self.addChild(darkLordTower)
         
         self.darkLordEye.name = "darkLordEye"
@@ -43,6 +56,7 @@ class MenuScene: SKScene {
         self.darkLordEye.anchorPoint = CGPoint(x: 0.5, y: 0.5)
         self.darkLordEye.position = CGPoint(x: self.darkLordTower.frame.midX, y: self.darkLordTower.frame.maxY * 0.94)
         self.darkLordEye.alpha = 0.0
+        self.darkLordEye.zPosition = 5
         self.addChild(darkLordEye)
         
         self.mountainsFront.name = "mountainsFront"
@@ -50,6 +64,7 @@ class MenuScene: SKScene {
         self.mountainsFront.size.height = UIScreen.main.bounds.maxY * 0.4
         self.mountainsFront.anchorPoint = CGPoint(x: 0, y: 0)
         self.mountainsFront.position = CGPoint(x: 0.0, y: 0.0)
+        self.mountainsFront.zPosition = 3
         self.addChild(mountainsFront)
         
         self.ground.name = "ground"
@@ -57,23 +72,97 @@ class MenuScene: SKScene {
         self.ground.size.height = UIScreen.main.bounds.maxY * 0.1
         self.ground.anchorPoint = CGPoint(x: 0, y: 0)
         self.ground.position = CGPoint(x: 0.0, y: 0.0)
+        self.ground.zPosition = 4
         self.addChild(ground)
+        
+        for i in 0...2 {
+            self.goblins[i].name = "goblin"
+            self.goblins[i].size = CGSize(width: 100, height: 100)
+            self.goblins[i].anchorPoint = CGPoint(x: 0.5, y: 0.5)
+            self.goblins[i].position = CGPoint(x: UIScreen.main.bounds.maxX + CGFloat(i*70), y: UIScreen.main.bounds.maxY * 0.1)
+            self.goblinSpawn[i] = self.goblins[i].position
+            self.goblins[i].zPosition = 4
+            self.addChild(self.goblins[i])
+        }
         
         self.darkSon.name = "darkSon"
         self.darkSon.size = CGSize(width: 233, height: 333)
         self.darkSon.anchorPoint = CGPoint(x: 0.5, y: 0.5)
         self.darkSon.position = CGPoint(x: UIScreen.main.bounds.maxX, y: UIScreen.main.bounds.maxY * 0.2)
+        self.darkSon.zPosition = 4
         self.addChild(darkSon)
-        
-        let endPosition = CGPoint(x: UIScreen.main.bounds.midX, y: UIScreen.main.bounds.maxY * 0.2)
-        let movingDarkSon = SKAction.move(to: endPosition, duration: 1.0)
-        self.darkSon.run(movingDarkSon)
         
         self.gameTitle.name = "gameTitle"
         self.gameTitle.anchorPoint = CGPoint(x: 0.5, y: 0.5)
         self.gameTitle.position = CGPoint(x: UIScreen.main.bounds.midX, y: UIScreen.main.bounds.maxY * 0.85)
+        self.gameTitle.zPosition = 100
         self.addChild(gameTitle)
         
+        let endDarkPosition = CGPoint(x: UIScreen.main.bounds.midX, y: UIScreen.main.bounds.maxY * 0.2)
+        
+        let endGoblinPosition = CGPoint(x: UIScreen.main.bounds.minX - 100, y: UIScreen.main.bounds.maxY * 0.1)
+        
+        let darkLordEyeAppear = SKAction.fadeIn(withDuration: 1.0)
+        
+        let fireParticle = SKEmitterNode(fileNamed: "FireParticle")
+        fireParticle!.name = "fireParticle"
+        fireParticle!.position = self.darkLordEye.position
+        fireParticle!.particleColorSequence = nil
+        fireParticle!.particleColorBlendFactor = 1.0
+        fireParticle!.particleColor = UIColor(red: 125/255, green: 15/255, blue: 204/255, alpha: 1.0)
+        fireParticle!.zPosition = 4
+        fireParticle!.particleScaleSpeed = -1.2
+        fireParticle!.setScale(2)
+        
+        let addFireParticle = SKAction.run({
+            self.addChild(fireParticle!)
+        })
+        
+        let darkEyeSequence = SKAction.sequence([
+            darkLordEyeAppear,
+            addFireParticle
+        ])
+        
+        let movingDarkSon = SKAction.move(to: endDarkPosition, duration: 1.0)
+        self.darkSon.run(movingDarkSon) {
+            self.darkLordEye.run(darkEyeSequence)
+            for i in 0...2 {
+                let movingGoblin = SKAction.move(to: endGoblinPosition, duration: self.getDuration(pointA: endGoblinPosition, pointB: self.goblinSpawn[i], speed: CGFloat.random(in: 150...250)))
+                self.isWalkingAnimation(goblin: self.goblins[i])
+                self.shouldFlip(oldPosition: self.goblinSpawn[i], newPosition: endGoblinPosition, node: self.goblins[i])
+                self.goblins[i].run(movingGoblin)
+            }
+        }
+    }
+    
+    private func getDuration(pointA: CGPoint, pointB: CGPoint, speed: CGFloat) -> TimeInterval {
+        let xDist = (pointB.x - pointA.x)
+        let yDist = (pointB.y - pointA.y)
+        let distance = sqrt((xDist * xDist) + (yDist * yDist));
+        let duration : TimeInterval = TimeInterval(distance/speed)
+        return duration
+    }
+
+    
+    public func isWalkingAnimation(goblin: SKSpriteNode) {
+        var walkTextures : [SKTexture] = []
+              
+        for i in 1...8 {
+            walkTextures.append(SKTexture(imageNamed: "normal_walk (\(i))"))
+        }
+        
+        let walkAnimation = SKAction.repeatForever(SKAction.animate(with: walkTextures, timePerFrame: 0.1))
+        
+        goblin.run(walkAnimation, withKey: "walkAnimation")
+    }
+    
+    public func shouldFlip(oldPosition: CGPoint, newPosition: CGPoint, node: SKSpriteNode) {
+        if Int(oldPosition.x) > Int(newPosition.x) {
+            node.xScale = -1
+        }
+        if Int(oldPosition.x) < Int(newPosition.x) {
+            node.xScale = 1
+        }
     }
     
     public func genocideFunction() {
@@ -121,6 +210,8 @@ struct MainScreenView: View {
     @State var isPressed: Bool = false
     @State var isPressedFX: Bool = false
     
+    @State private var textOpacity = 1.0
+    
     var body: some View {
         GeometryReader { geometry in
             ZStack {
@@ -141,16 +232,23 @@ struct MainScreenView: View {
                     //                Spacer()
                     
                     Button {
-                        withAnimation { self.startGame() }
+                        self.startGame()
                     } label: {
                         Text("Play")
-                            .font(.custom("Chalkduster", size: (UIDevice.current.userInterfaceIdiom == .pad ? 30 : 15)))
+                            .font(.custom("Nightmare", size: (UIDevice.current.userInterfaceIdiom == .pad ? 150 : 15)))
+                            .opacity(textOpacity)
+                            .onAppear{
+                                withAnimation{
+                                    textOpacity = 0.0
+                                }
+                            }
+                            .animation(Animation.easeInOut(duration:1.5).repeatForever(autoreverses:true))
                         
                     }
-                    .frame(maxWidth: geometry.size.width * 0.1, maxHeight: geometry.size.height * 0.1)
+                    .frame(maxWidth: geometry.size.width * 0.5, maxHeight: geometry.size.height * 0.3)
                     .padding(5)
-                    .foregroundColor(.white)
-                    .background(.green)
+                    .foregroundColor(.green)
+                    .background(.clear)
                     .cornerRadius(15.0)
                     
                 }
